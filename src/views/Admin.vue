d7bc30146f44a19fd0bdec04e769fb26b2caabd2
Darron Park <darron1217@gmail.com>
2019-10-23 13:10:08 +0900

    관리자 홈 UI변경

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -14,32 +14,22 @@
       </ion-toolbar>
     </ion-header>
     <ion-content>
-      <ion-grid>
-        <ion-row>
-          <ion-col>
-            <ion-card>
-              <ion-card-header>
-                <ion-card-title>푸드트럭</ion-card-title>
-              </ion-card-header>
-
-              <ion-card-content>
-                1대
-              </ion-card-content>
-            </ion-card>
-          </ion-col>
-          <ion-col>
-            <ion-card>
-              <ion-card-header>
-                <ion-card-title>준비중 상품</ion-card-title>
-              </ion-card-header>
-
-              <ion-card-content>
-                5건
-              </ion-card-content>
-            </ion-card>
-          </ion-col>
-        </ion-row>
-      </ion-grid>
+      <ion-card>
+        <ion-card-header>
+          <ion-card-title>푸드트럭</ion-card-title>
+        </ion-card-header>
+        <ion-card-content>
+          1대
+        </ion-card-content>
+      </ion-card>
+      <ion-card>
+        <ion-card-header>
+          <ion-card-title>준비중인 상품</ion-card-title>
+        </ion-card-header>
+        <ion-card-content>
+          5건
+        </ion-card-content>
+      </ion-card>
     </ion-content>
   </ion-page>
 </template>
